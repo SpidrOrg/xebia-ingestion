09e647dec0a666f808bdd0ef1926986ca5b92b94
visdxebia <vishal.daga@xebia.com>
2023-05-30 15:57:19 +0530

    sdf

--- putValue.js
+++ putValue.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+const { exec } = require('node:child_process');
 // This
 const indexOfAwsAccountInArnSplit = process.env.CODEBUILD_BUILD_ARN.split(":").indexOf(process.env.AWS_REGION) + 1;
 const awsAccount = process.env.CODEBUILD_BUILD_ARN.split(":")[indexOfAwsAccountInArnSplit];
@@ -9,41 +9,74 @@ const awsRegion = process.env.AWS_REGION;
 // const awsAccount = "319925118739";
 // const awsRegion = "us-east-1";
 
-const TERRAFORM_STATEFILE_BUCKET = process.env.TERRAFORM_STATEFILE_BUCKET;
-const TERRAFORM_STATELOCK_DD_TABLE = process.env.TERRAFORM_STATELOCK_DD_TABLE;
 
-const TOKENS = {
-//   ":TERRAFORM_STATEFILE_BUCKET:": TERRAFORM_STATEFILE_BUCKET,
-//   ":TERRAFORM_STATELOCK_DD_TABLE:": TERRAFORM_STATELOCK_DD_TABLE,
-//   ":ENV_NAME:": "stage",
-//   ":ENVIRONMENT123123:": process.env.ENV_NAME,
-  ":123456789012:": awsAccount
-}
-
-console.log(TOKENS)
-const replaceFilePath = path.join(__dirname, "./buildspec/buildspec-stage.yml");
-let fileContents = fs.readFileSync(replaceFilePath, "utf-8");
-Object.keys(TOKENS).forEach(token=>{
-  fileContents = fileContents.replaceAll(token, TOKENS[token]);
-})
-fs.writeFileSync(replaceFilePath, fileContents);
-
-function recursivelyReplaceTokens(startPath){
-  const foldersOrFiles = fs.readdirSync(startPath).filter(item => !/(^|\/)\.[^/.]/g.test(item))
-  foldersOrFiles.forEach((folderOrFile)=>{
-    const folderOrFilePath = path.join(startPath, folderOrFile)
-    const isFolder = fs.lstatSync(folderOrFilePath).isDirectory();
-    if (isFolder){
-      recursivelyReplaceTokens(folderOrFilePath);
-    } else {
-      let fileContents = fs.readFileSync(folderOrFilePath, "utf-8");
-      Object.keys(TOKENS).forEach(token=>{
-        fileContents = fileContents.replaceAll(token, TOKENS[token]);
-      })
-      fs.writeFileSync(folderOrFilePath, fileContents);
-    }
-  })
-}
-
-const pathToInfraFolder = path.join(__dirname, "./buildspec");
-recursivelyReplaceTokens(pathToInfraFolder);
+///
+exec(`export bucket_name=${awsAccount}-codebase`);
+exec(`export lambda_functions=[
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/moodys-monthly",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:krny-moodys-monthly"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/moodys-quarterly",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:krny-moodys-quarterly"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/moodys-yearly",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:krny-moodys-yearly"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/moodys-manual",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:krny-moodys-188"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/covid",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-covid"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/yahoo_function",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-yahoofin"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/fred",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-fred"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/googletrendschild",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-googletrends-child"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/googletrendsclient",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-googletrends-client"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/googletrendsparent",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-googletrends-parent"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/ihs",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-ihs"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/similar-web",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-similarweb"
+  },
+  {
+    "name": "lambda_function.py",
+    "folder": "functions/similar-web-client",
+    "arn": "arn:aws:lambda:${awsRegion}:${awsAccount}:function:ingestion-similarweb-client"
+  }
+]
+`)
